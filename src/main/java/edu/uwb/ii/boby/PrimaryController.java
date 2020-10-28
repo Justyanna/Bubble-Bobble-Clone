6c16dc7c1b060af8a85829f2780a70d683077363
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2020-10-28 10:46:05 +0100

    initial commit

---
+++ src/main/java/edu/uwb/ii/boby/PrimaryController.java
@@ -0,0 +1,13 @@
+package edu.uwb.ii.boby;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/edu/uwb/ii/boby/SecondaryController.java
@@ -0,0 +1,13 @@
+package edu.uwb.ii.boby;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
