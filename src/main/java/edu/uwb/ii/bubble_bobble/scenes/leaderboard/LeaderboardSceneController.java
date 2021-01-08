2451d1a04c364f47bfd1f27825a397f126c6d21c
Justyanna <annajkuczynska@onet.pl>
2021-01-08 01:00:26 +0100

    Add different leaderboard localisation

--- src/main/java/edu/uwb/ii/bubble_bobble/scenes/leaderboard/LeaderboardSceneController.java
+++ src/main/java/edu/uwb/ii/bubble_bobble/scenes/leaderboard/LeaderboardSceneController.java
@@ -1,30 +1,22 @@
 package edu.uwb.ii.bubble_bobble.scenes.leaderboard;
 
 import edu.uwb.ii.bubble_bobble.App;
-import edu.uwb.ii.bubble_bobble.utils.EncryptionProvider;
-import edu.uwb.ii.bubble_bobble.utils.exceptions.EncryptionException;
+import edu.uwb.ii.bubble_bobble.utils.LeaderboardFileManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 
-import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.Objects;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class LeaderboardSceneController {
 
     private static final Logger LOGGER = Logger.getLogger(LeaderboardSceneController.class.getName());
-    private static final String LEADERBOARD_PATH = "leaderboard/leaderboard.txt";
-    private static final String ENCRYPTION_KEY = "Okon1234Okon1234";
 
     @FXML
     TableView<LeaderBoardData> tableView;
@@ -34,26 +26,9 @@ public class LeaderboardSceneController {
     TableColumn<LeaderBoardData, Number> colScore;
     @FXML
     TableColumn<LeaderBoardData, String> colDate;
-
-    public static void saveScore(String name, String score, String date) {
-        try {
-            File file = new File(Objects.requireNonNull(
-                    LeaderboardSceneController.class.getClassLoader().getResource(LEADERBOARD_PATH))
-                                         .getFile());
-            EncryptionProvider.decrypt(ENCRYPTION_KEY, file, file);
-
-            try (FileWriter fw = new FileWriter(file, true); BufferedWriter bw = new BufferedWriter(fw);
-                 PrintWriter pw = new PrintWriter(bw)) {
-
-                pw.println(name + " " + score + " " + date);
-                pw.flush();
-            }
-
-            EncryptionProvider.encrypt(ENCRYPTION_KEY, file, file);
-        } catch (EncryptionException | IOException e) {
-            LOGGER.warning("Cannot save score on leaderboard");
-        }
-    }
+    @FXML
+    VBox vbox;
+    LeaderboardFileManager leaderboardFileManager;
 
     @FXML
     private void switchToMenu() throws IOException {
@@ -61,54 +36,29 @@ public class LeaderboardSceneController {
     }
 
     public void initialize() {
+        leaderboardFileManager = new LeaderboardFileManager();
         prepareTableView();
+        leaderboardFileManager.saveScore("Ania","5000", "10.10.2020");
     }
 
     private TableView<LeaderBoardData> prepareTableView() {
-        Collection<LeaderBoardData> list = getLeaderBoardData();
-        ObservableList<LeaderBoardData> details = FXCollections.observableArrayList(list);
+        Collection<LeaderBoardData> list = leaderboardFileManager.getLeaderBoardData();
 
-        colName.setCellValueFactory(data -> data.getValue().nameProperty());
-        colScore.setCellValueFactory(data -> data.getValue().scoreProperty());
-        colDate.setCellValueFactory(data -> data.getValue().dateProperty());
+        if (list != null && (!list.isEmpty())) {
+            ObservableList<LeaderBoardData> details = FXCollections.observableArrayList(list);
 
-        tableView.setEditable(false);
-        tableView.setItems(details);
+            colName.setCellValueFactory(data -> data.getValue().nameProperty());
+            colScore.setCellValueFactory(data -> data.getValue().scoreProperty());
+            colDate.setCellValueFactory(data -> data.getValue().dateProperty());
+
+            tableView.setEditable(false);
+            tableView.setItems(details);
+        } else {
+            vbox.getChildren().remove(tableView);
+            Label label = new Label("Leaderboard is empty :( Come back here soon...");
+            vbox.getChildren().add(0, label);
+        }
 
         return tableView;
-    }
-
-    private Collection<LeaderBoardData> getLeaderBoardData() {
-        Collection<LeaderBoardData> list = null;
-
-        try {
-
-            File file = new File(getClass().getClassLoader().getResource(LEADERBOARD_PATH).getFile());
-//            EncryptionProvider.decrypt(ENCRYPTION_KEY, file, file);
-            URL path = getClass().getClassLoader().getResource(LEADERBOARD_PATH);
-
-            list = Files.readAllLines(Paths.get(path.toURI())).stream().map(line -> {
-                String[] details = line.split(" ");
-                return new LeaderBoardData(details[0], details[1], details[2]);
-            }).collect(Collectors.toList());
-
-//            EncryptionProvider.encrypt(ENCRYPTION_KEY, file, file);
-        } catch (IOException | URISyntaxException e) {
-            LOGGER.warning("Cannot load leaderboard");
-        }
-
-        assert list != null;
-        if (list.size() < 31) {
-            list = list.stream()
-                    .sorted(Comparator.comparingInt(LeaderBoardData::getScore).reversed())
-                    .collect(Collectors.toList())
-                    .subList(0, list.size());
-        } else {
-            list = list.stream()
-                    .sorted(Comparator.comparingInt(LeaderBoardData::getScore).reversed())
-                    .collect(Collectors.toList())
-                    .subList(0, 31);
-        }
-        return list;
     }
 }
---
+++ src/main/java/edu/uwb/ii/bubble_bobble/utils/LeaderboardFileManager.java
@@ -0,0 +1,130 @@
+package edu.uwb.ii.bubble_bobble.utils;
+
+import edu.uwb.ii.bubble_bobble.scenes.leaderboard.LeaderBoardData;
+import edu.uwb.ii.bubble_bobble.utils.exceptions.EncryptionException;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class LeaderboardFileManager {
+
+    private static final Logger LOGGER = Logger.getLogger(LeaderboardFileManager.class.getName());
+    private static final String ENCRYPTION_KEY = "Okon1234Okon1234";
+    private final String appPath;
+    private final String leaderboardPath;
+
+    public LeaderboardFileManager() {
+        appPath = System.getProperty("user.home") + "/AppData/Local/Bubble Bobble Clone/leaderboard";
+        leaderboardPath = appPath + "/leaderboard.txt";
+    }
+
+    public void saveScore(String name, String score, String date) {
+        try {
+            File file = new File(leaderboardPath);
+            EncryptionProvider.decrypt(ENCRYPTION_KEY, file, file);
+
+            try (FileWriter fw = new FileWriter(file, true); BufferedWriter bw = new BufferedWriter(fw);
+                 PrintWriter pw = new PrintWriter(bw)) {
+
+                pw.println(name + " " + score + " " + date);
+                pw.flush();
+            }
+
+            EncryptionProvider.encrypt(ENCRYPTION_KEY, file, file);
+        } catch (EncryptionException | IOException e) {
+            LOGGER.warning("Cannot save score on leaderboard");
+        }
+    }
+
+    public Collection<LeaderBoardData> getLeaderBoardData() {
+        Collection<LeaderBoardData> list = null;
+
+        try {
+            String[] lines = readFile();
+            if (lines.length > 0) {
+                list = createLeaderBoardDataCollection(lines);
+            }
+        } catch (IOException | NullPointerException | EncryptionException e) {
+            LOGGER.warning("Cannot load leaderboard");
+            return null;
+        }
+
+        list = takeSublist(list);
+        return list;
+    }
+
+    private String[] readFile() throws IOException, EncryptionException {
+        String contents;
+        try {
+            Files.createDirectories(Path.of(appPath));
+            Files.createFile(Path.of(leaderboardPath));
+        } catch (IOException e) {
+            LOGGER.info("Leaderboard directory already exist");
+        }
+
+        File file = new File(leaderboardPath);
+        tryDecryptLeaderboardFile(file);
+
+        try (InputStream inputStream = new FileInputStream(file);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            contents = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        } catch (Exception e) {
+            throw e;
+        }
+
+        tryEncryptLeaderboardFile(file);
+        return contents.split(System.lineSeparator());
+    }
+
+    private Collection<LeaderBoardData> createLeaderBoardDataCollection(String[] lines) {
+        Collection<LeaderBoardData> list;
+        list = Arrays.stream(lines).sequential().map(line -> {
+            if (!line.isEmpty()) {
+                String[] details = line.split(" ");
+                return new LeaderBoardData(details[0], details[1], details[2]);
+            }
+
+            throw new NullPointerException();
+        }).collect(Collectors.toList());
+        return list;
+    }
+
+    private Collection<LeaderBoardData> takeSublist(Collection<LeaderBoardData> list) {
+        if (list.size() < 31) {
+            list = list.stream()
+                    .sorted(Comparator.comparingInt(LeaderBoardData::getScore).reversed())
+                    .collect(Collectors.toList())
+                    .subList(0, list.size());
+        } else {
+            list = list.stream()
+                    .sorted(Comparator.comparingInt(LeaderBoardData::getScore).reversed())
+                    .collect(Collectors.toList())
+                    .subList(0, 20);
+        }
+        return list;
+    }
+
+    private void tryEncryptLeaderboardFile(File file) throws EncryptionException {
+        try {
+            EncryptionProvider.encrypt(ENCRYPTION_KEY, file, file);
+        } catch (EncryptionException e) {
+            LOGGER.info("Cannot decrypt file");
+            throw e;
+        }
+    }
+
+    private void tryDecryptLeaderboardFile(File file) throws EncryptionException {
+        try {
+            EncryptionProvider.decrypt(ENCRYPTION_KEY, file, file);
+        } catch (EncryptionException e) {
+            LOGGER.info("Cannot decrypt file");
+            throw e;
+        }
+    }
+}
